befe7ca65045849e07d4ac96ceda6d0632a6af22
Nick Cook <nickcook530@gmail.com>
2019-01-01 21:56:48 +0000

    Completed Arrays chapter exercises

---
+++ 6_Arrays/Arrays_1.rb
@@ -0,0 +1,11 @@
+arr1 = [1, 3, 5, 7, 9, 11]
+number = 3
+puts arr1.include?(number)
+
+arr2 = [["test", "hello", "world"],["example", "mem"]]
+puts arr2.last.first
+
+arr3 = [3, 4, 5, 6, 7]
+arr4 = arr3.map{|x| x+2}
+p arr3
+p arr4
